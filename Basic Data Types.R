616efe0b0170a1e39666fdc113de4213276c82af
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-08 20:27:49 +0530

    Update Basic Data Types.R

--- Basic Data Types.R
+++ Basic Data Types.R
@@ -1,31 +1,47 @@
+
 # R BASIC DATA TYPES
 
 # First We Start with the Numerics data type
 
 # Numerics
+
 x <- 10
+class(x)
+typeof(x)
 
 # Integers 
+
 # Natural (whole) numbers are known as integers and are also part of the numeric class
 
-i <- 5
 a <- 2L
+typeof(a)
+class(a)
 
-
-# Here we assign the value 2L to a variable a  
-#We use 2L Because by default R will decide for you how should store the data
-# All the Arithmetic opeation are always conducted on double level because
-# so if you not going to run the arithmetic opeartion and you are not going to add them or creating them then you can use L 
-which stores in integer format
+# We use 2L Because by default R will decide for you how should store the data.
+# All the Arithmetic opeation are always conducted on double level because if you are not going to run the arithmetic opeartion 
+# and you are not going to add them or creating them then you can use L which stores in integer format
 
 # Double
+
 y <- 2.5
 
+typeof(y)
+class(y)
+
+
 # Logicals
-# Boolean values (True and False) are part of the logical class. In R these are written in All Caps.
+
+# Boolean values (True and False) are part of the logical class.
 
 t <- TRUE
+
+typeof(t)
+class(t)
+
 f <- FALSE
+
+typeof(f)
+class(f)
 
 # Characters
 # Text/string values are known as characters in R. You use quotation marks to create a text character string:
@@ -33,35 +49,14 @@ f <- FALSE
 char <- "R is programming Language"
 char
 
+typeof(char)
+class(char)
+
 # Complex
 z<-3+2i
-
+typeof(z)
+class(z)
 
 # Checking Data Type Classes
 # You can use the class() function to check the data type of a variable:
-
-class(x)
-
-class(i)
-
-class(a)
-
-class(y)
-
-class(t)
-
-class(f)
-
-#typeof determines the (R internal) type or storage mode of any object
-
-typeof(x)
-
-typeof(i)
-
-typeof(a)
-
-typeof(y)
-
-typeof(t)
-
-typeof(f)
+# typeof determines the (R internal) type or storage mode of any object
