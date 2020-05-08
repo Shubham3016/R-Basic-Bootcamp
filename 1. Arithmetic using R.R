c9178c2bbafefcbbd49568fb68df1829cd19bede
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-08 18:34:28 +0530

    Add files via upload

---
+++ 1. Arithmetic using R.R
@@ -0,0 +1,19 @@
+# Basics of R
+
+1+1   # Addition
+
+5-2   # Subtraction 
+
+3*6   # Multiplication
+
+10/5  # Division
+
+3**2  # 3 square 
+
+3**3  # 3 cube
+
+5 %% 2 # Modulo
+
+2^3  # Exponents
+
+(100 * 2) + (50 / 2) # Order of Operations
