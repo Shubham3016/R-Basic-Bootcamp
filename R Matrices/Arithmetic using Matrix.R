00c3d8e37b716d6a5f5bd008e4e7672b49687d56
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-09 17:10:15 +0530

    Update Arithmetic using Matrix.R

--- R Matrices/Arithmetic using Matrix.R
+++ R Matrices/Arithmetic using Matrix.R
@@ -1,1 +1,36 @@
+# Basic Arithmetic Operation using Matrices
+
+v <- matrix(1:20,byrow=TRUE,nrow=4)
+v
+
+# Multiplication
+
+v * v
+
+2*v
+
+# Addition
+
+v + v
+
+# Division
+
+v / v
+
+v / 2
+
+# Division (reciprocal)
+
+1 / v
+
+# Power
+
+v ^ 2
+
+
+# Comparison operators with matrices
+
+v > 17
+
+v < 25
 
