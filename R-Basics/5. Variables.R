4cfaa314d145754c5d79df7028a3f37d2633642e
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-09 10:23:11 +0530

    Add files via upload

---
+++ R-Basics/5. Variables.R
@@ -0,0 +1,26 @@
+# Variables
+
+# You can use the <- character to assign a variable, 
+
+#  Use hashtags for comments
+
+variable.name <- 100
+
+variable.name
+
+# Working with variables
+# We can use variables together and work with them, for example:
+
+bank.account <- 100
+
+deposit <- 10
+
+bank.account <- bank.account + deposit
+
+bank.account
+
+
+x<-2  # <- Comparision Operator to set value
+y<-4
+z<-6
+x+y+z
