256d588e44fa6f70754a88cd1227af5fffdda23a
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-09 10:41:13 +0530

    Add files via upload

---
+++ R-Basics/5. Variables.R
@@ -0,0 +1,47 @@
+# Variables
+
+# You can use the ( <- ) character to assign a variable, 
+
+#  Use hashtags for comments
+
+# valid & invalid variable types
+
+# Valid
+
+# only takes character,number, _ , .
+
+var_name. <- 1 # Has letters, numbers, dot and underscore
+.var_name <- 2 # Can start with a dot(.) but the dot(.)should not be followed by a number
+var.name <- 3
+
+# Invalid 
+
+# var_name% <- 4 # Only dot(.) and underscore allowed 
+# 2var_name <- 5 # cannot start with a number
+# .2var_name <- 6 # dot followed by a number makes it invalid
+# _var_name <- 7 # cannot start with _
+
+name <- 100
+
+name
+
+# Working with variables
+# We can use variables together and work with them, for example:
+
+price.stock <- 100
+
+sell <- 10
+
+price.stock <- price.stock + sell
+
+price.stock
+
+
+x<-2  # <- Basic calculation 
+y<-4
+z<-6
+x+y+z
+© 2020 GitHub, Inc.
+Terms
+Privacy
+S
