3a273b23139dee372bd71f8e141568db6d1c7d9c
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-09 10:36:21 +0530

    Update 5. Variables.R

--- R-Basics/5. Variables.R
+++ R-Basics/5. Variables.R
@@ -1,26 +1,43 @@
 # Variables
 
-# You can use the <- character to assign a variable, 
+# You can use the ( <- ) character to assign a variable, 
 
 #  Use hashtags for comments
 
-variable.name <- 100
+# valid & invalid variable types
 
-variable.name
+# Valid
+
+# only takes character,number, _ , .
+
+var_name. <- 1 # Has letters, numbers, dot and underscore
+.var_name <- 2 # Can start with a dot(.) but the dot(.)should not be followed by a number
+var.name <- 3
+
+# Invalid 
+
+var_name% <- 4 # Only dot(.) and underscore allowed 
+2var_name <- 5 # cannot start with a number
+.2var_name <- 6 # dot followed by a number makes it invalid
+_var_name <- 7 # cannot start with _
+
+name <- 100
+
+name
 
 # Working with variables
 # We can use variables together and work with them, for example:
 
-bank.account <- 100
+price.stock <- 100
 
-deposit <- 10
+sell <- 10
 
-bank.account <- bank.account + deposit
+price.stock <- price.stock + sell
 
-bank.account
+price.stock
 
 
-x<-2  # <- Comparision Operator to set value
+x<-2  # <- Basic calculation 
 y<-4
 z<-6
 x+y+z
