6b123ad95e33b21fae747a275f4c93947c6c376f
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-10 21:49:23 +0530

    Update 2. Basic Arithmetic Operations.R

--- R-Basics/2. Basic Arithmetic Operations.R
+++ R-Basics/2. Basic Arithmetic Operations.R
@@ -1,19 +1,37 @@
-# Basics of R
+ # Basics of R
 
 2+6   # Addition of Two Numbers
 
+# [1] 8
+ 
 5-2   # Subtraction of Two Numbers
 
+#[1] 3
+ 
 3*6   # Multiplication of Two Numbers
+
+# [1] 18
  
 10/5  # Division of Two Numbers
 
+[1] 2
+
 3**2  # Square of 3
+
+# [1] 9
 
 3**3  # Cube of 3
 
+# [1] 27
+
 5 %% 2 # Modulo (Gives Remainder)
+
+# [1] 1
 
 2^3  # Exponents 2*2*2=8
 
+# [1] 8
+
 (10 * 2) + (5 / 2) # Order of Operations
+
+# [1] 22.5
