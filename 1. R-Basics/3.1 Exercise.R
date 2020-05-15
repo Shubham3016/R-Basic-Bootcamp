0bf5f231a072a27ce398c5dc0a5b118b00345080
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-15 11:41:13 +0530

    Add files via upload

---
+++ 1. R-Basics/3.1 Exercise.R
@@ -0,0 +1,26 @@
+# Create a vector x storing the sequence 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20.
+
+x<-c(10,11,12,13,14,15,16,17,18,19,20,21)
+
+# Add 2 to it. Save the result into a variable called y.
+y<-x+2
+y
+
+# Multiply y by 3. Save that into a variable called z.
+z<-y*3
+z
+
+# Subtract 6 from z and divide the result by 3. Save what you get into a variable called answer.
+z-6/3
+z
+
+# Print your answer variable.
+
+x+2*3-6/3
+x+6-2
+x+4
+
+#Try to do the entire operation in a single line of code. 
+
+x+(2*3)-(6/3)
+
