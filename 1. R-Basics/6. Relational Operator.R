da714146821136e0cbcf71dc4d416c1c2e0de861
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-15 12:45:13 +0530

    Update 6. Relational Operator.R

--- 1. R-Basics/6. Relational Operator.R
+++ 1. R-Basics/6. Relational Operator.R
@@ -36,45 +36,62 @@ F==F
 'dog'=='dog'
 # [1] TRUE
 
-'cat'!='cat'
+
+# != Not equal to  (exclamation marks)
+
+'cat' != 'cat'
 # [1] FALSE
 
-'car'!='dog'
+'car' != 'dog'
 # [1] TRUE
 
-'T'!=F
+'T' != F
 # [1] TRUE
 
-'T'!='T'
+'T' != 'T'
 # [1] FALSE
 
 'sugar'!='salt'
 # [1] TRUE
+
+
+# GREATER THEN 
+
+ 17 > 21
+# [1] FALSE
+
+21 > 21
+# [1] FALSE
+
+21 > 20
+# [1] TRUE
+
+
+# LESS THEN
+
+20 < 1
+# [1] FALSE
+
+20 < 22  
+# [1] TRUE
+
+# T=1 F=0 1<0 cercion rule
+
+T < F         
+# [1] FALSE
+
+# GREATER THEN EQUAL TO
+
+7 >= 4
+# [1] TRUE
  
- 17>21
-# [1] FALSE
-
-21>21
-# [1] FALSE
-
-21>20
+7 >= 7
 # [1] TRUE
 
-20<1
-# [1] FALSE
+ # LESS THEN EQUAL TO
 
-20<22  
-# [1] TRUE
-
-T<F             # T=1 F=0 1<0 cercion rule
-# [1] FALSE
-
-7>=4
-# [1] TRUE
- 
-7>=7
-# [1] TRUE
-
-7<=4
+7 <= 4
 # [1] FALSE
  
+5 <= 8
+# [1] TRUE
