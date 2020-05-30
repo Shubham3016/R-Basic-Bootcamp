a43d8f6e966da38afd9994a57eb18bbe36905830
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-05-30 11:07:13 +0530

    Create 4.Importing and saving data from Excel files.R

---
+++ 4. Importing Data In R/4.Importing and saving data from Excel files.R
@@ -0,0 +1,25 @@
+# Excel files are very popular among data analysts. Spreadsheets are easy to work with and flexible. 
+
+# R is equipped with a library readxl to import Excel spreadsheet.
+
+# To do this, we need the readxl package for R. Remember you can download it by using:
+
+install.packages('readxl')
+
+# Load the readxl package
+
+library(readxl)
+
+# list the sheets of the excel file
+
+excel_sheets('D:\\Clustering\\EastWestAirlines.xlsx')
+
+# used read_excel to read excel dataset
+
+df <- read_excel('D:\\Clustering\\EastWestAirlines.xlsx',sheet='Sheet1')
+
+# If you had multiple sheets that you wanted to import into a list, you could do this with lapply():
+
+entire_workbook <- lapply(excel_sheets("D:\\Clustering\\EastWestAirlines.xlsx"), 
+                      read_excel, 
+                      path = "D:\\Clustering\\EastWestAirlines.xlsx")
