4260e23b8ff3484326342dec4a1efff25f1cb033
Shubham More <43364183+Shubham3016@users.noreply.github.com>
2020-06-05 11:09:28 +0530

    Create Regular expression.R

---
+++ 9. Function  in R/Regular expression.R
@@ -0,0 +1,28 @@
+# Regular Expressions
+
+# Regular expressions is a general term which covers the idea of pattern searching, typically in a string (or a vector of strings).
+
+# grepl(), which returns a logical indicating if the given word is found
+
+# grep(), which returns a vector of index locations of matching pattern instances
+
+text <- "Hi there, do you know who you are voting for?"
+
+?grep
+
+grepl('voting',text)   # in the above text variable word voting is present so it will return TRUE
+# [1] TRUE
+
+grepl('bob',text)      # in the above text variable word bob is not present so it will return FALSE
+# [1] FALSE
+
+v <- c('a','b','c','d')
+v
+# [1] "a" "b" "c" "d"
+ 
+grep('a',v)
+# [1] 1
+
+grep('q',v)
+integer(0)
+ 
